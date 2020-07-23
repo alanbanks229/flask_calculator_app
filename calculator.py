0a98df672ecb91d2ee852c099755baa9a9ef386b
alanbanks229 <alanbanks229@gmail.com>
2020-07-23 15:56:30 -0400

    refactored code again, easy to read now

--- calculator.py
+++ calculator.py
@@ -7,51 +7,56 @@ from flask import Flask, render_template, request
 Flask_App = Flask(__name__) # Creating our Flask Instance
 
 
-@Flask_App.route('/')
+@Flask_App.route('/', methods=['GET'])
 def index():
-    """
-    Displays the index page accessible at '/'
-    """
+    """ Displays the index page accessible at '/' """
 
     return render_template('index.html')
 
 
 @Flask_App.route('/operation_result/', methods=['POST'])
 def operation_result():
+    """Route where we send calculator form input"""
 
     error = None
     result = None
-    # we need below becaue 0 is seen as falsey
     calculation_success = False
+
     try:
-        # looks for html tags with specified name=
+        # request.form looks for:
+        # html tags with matching "name= "
         input1 = float(request.form['Input1'])
         input2 = float(request.form['Input2'])
         operation = request.form['operation']
 
-        #operation on default is addition.
+        #On Default, the operation on webpage is addition
         if operation == "add":
             result = add(input1, input2)
             operation = "plus "
             calculation_success = True
+
         elif operation == "sub":
             result = subtract(input1, input2)
             operation = "minus "
             calculation_success = True
+
+        # this will throw ZeroDivisionError if input2 is 0
         elif operation == "div":
             operation = "divided by "
+            result = input1 / input2 
+            #calculation_success = True will not be hit if error occurs above.
             calculation_success = True
-            # this will throw ZeroDivisionError if input2 is 0
-            result = input1 / input2
-            calculation_success = True
+
         elif operation == "mult":
             result = multiply(input1, input2)
             operation = "multiplied by "
             calculation_success = True
+
         else:
             result = mod(input1, input2)
             operation = "modulo "
             calculation_success = True
+
         return render_template(
             'index.html',
             input1=input1,
@@ -78,6 +83,7 @@ def operation_result():
             error="Cannot perform numeric operations with provided input"
         )
 
+# Our helper methods.
 def add(input1, input2):
     answer = input1 + input2
     print("The addition of", input1, "and", input2, "is", answer)
