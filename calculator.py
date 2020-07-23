368e235ae86e05d8a6ab86f91bca6d579516f49c
alanbanks229 <alanbanks229@gmail.com>
2020-07-23 15:37:43 -0400

    refactored code, less conditionals, using try catch

--- calculator.py
+++ calculator.py
@@ -19,37 +19,31 @@ def index():
 @Flask_App.route('/operation_result/', methods=['POST'])
 def operation_result():
 
-    # looks for html tags with specified name=
-    input1 = request.form['Input1']
-    input2 = request.form['Input2']
-    operation = request.form['operation']
+    error = None
     result = None
-    # conditional to check if input1 AND input2 are instances int or float.
-    # also checking if operation is not null.
-    if (isinstance(input1, (int, long)) and 
-        isinstance(input2, (int, long)) and
-        operation):
+    try:
+        # looks for html tags with specified name=
+        input1 = float(request.form['Input1'])
+        input2 = float(request.form['Input2'])
+        operation = request.form['operation']
+
+        #operation on default is addition.
         if operation == "add":
             result = add(input1, input2)
+            operation = "plus "
         elif operation == "sub":
             result = subtract(input1, input2)
+            operation = "minus "
         elif operation == "div":
-            try:
-                result = input1 / input2
-            except ZeroDivisionError:
-                error = "Warning: dividing by zero is not allowed"
-                return render_template(
-                    'index.html',
-                    input1=input1,
-                    input2=input2,
-                    operation=operation,
-                    error=error
-                )
+            # this will throw ZeroDivisionError if input2 is 0
+            result = input1 / input2
+            operation = "divided by "
         elif operation == "mult":
             result = multiply(input1, input2)
+            operation = "multiplied by "
         else:
             result = mod(input1, input2)
-        
+            operation = "modulo "
         return render_template(
             'index.html',
             input1=input1,
@@ -57,14 +51,23 @@ def operation_result():
             operation=operation,
             result=result
         )
-    # else
-    return render_template(
-        'index.html',
-        error="invalid input",
-        input1=input1,
-        input2=input2,
-        operation=operation
-    )
+        
+    except ZeroDivisionError:
+        return render_template(
+            'index.html',
+            input1=input1,
+            input2=input2,
+            operation=operation,
+            error="You cannot divide by zero"
+        )
+    except ValueError:
+        return render_template(
+            'index.html',
+            input1=input1,
+            input2=input2,
+            operation=operation,
+            error="Cannot perform numeric operations with provided input"
+        )
 
 def add(input1, input2):
     answer = input1 + input2
